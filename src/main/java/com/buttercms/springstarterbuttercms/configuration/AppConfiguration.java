e413b19fddda6dee2651452672c93f0b4448adc6
Jan Štefánik <jan.stefanik@applifting.cz>
2022-03-02 13:05:32 +0100

    Load API key from .env file

--- src/main/java/com/buttercms/springstarterbuttercms/configuration/AppConfiguration.java
+++ src/main/java/com/buttercms/springstarterbuttercms/configuration/AppConfiguration.java
@@ -1,2 +1,19 @@
-package com.buttercms.springstarterbuttercms.configuration;public class AppConfiguration {
+package com.buttercms.springstarterbuttercms.configuration;
+
+import com.buttercms.ButterCMSClient;
+import com.buttercms.IButterCMSClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfiguration {
+
+    @Value("${buttercms.token}")
+    private String token;
+
+    @Bean
+    public IButterCMSClient butterCMSClient() {
+        return new ButterCMSClient(token);
+    }
 }
