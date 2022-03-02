16934e69559a367b466d0a21d0a0e35f8c5730eb
Jan Štefánik <jan.stefanik@applifting.cz>
2022-03-02 10:53:14 +0100

    Initialize empty project structure

---
+++ src/main/java/com/buttercms/springstarterbuttercms/SpringStarterButtercmsApplication.java
@@ -0,0 +1,13 @@
+package com.buttercms.springstarterbuttercms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterButtercmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStarterButtercmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/buttercms/springstarterbuttercms/SpringStarterButtercmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buttercms.springstarterbuttercms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterButtercmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
