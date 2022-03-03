e0ead073b662d83430ca37be935a56fb8374cd8c
Jan Štefánik <jan.stefanik@applifting.cz>
2022-03-03 09:58:34 +0100

    Make the featured blog posts on home page fetch butter cms' data via the SDK

---
+++ src/main/java/com/buttercms/springstarterbuttercms/controller/BlogController.java
@@ -0,0 +1,2 @@
+package com.buttercms.springstarterbuttercms.controller;public class BlogController {
+}
--- src/main/java/com/buttercms/springstarterbuttercms/controller/IndexController.java
+++ src/main/java/com/buttercms/springstarterbuttercms/controller/IndexController.java
@@ -1,9 +1,14 @@
 package com.buttercms.springstarterbuttercms.controller;
 
 import com.buttercms.IButterCMSClient;
+import com.buttercms.model.PostsResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Controller
 public class IndexController {
@@ -15,6 +20,12 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model) {
+        Map<String, String> queryParams = new HashMap<>() {{
+            put("page", "1");
+            put("page_size", "2");
+        }};
+        PostsResponse posts = butterCMSClient.getPosts(queryParams);
+        model.addAttribute("posts", posts.getData());
         return "index";
     }
 }
