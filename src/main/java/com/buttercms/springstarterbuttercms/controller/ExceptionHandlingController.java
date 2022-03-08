7cd3c88e595c24ef5ab7a367447ece1da07d3e08
Jan Štefánik <jan.stefanik@applifting.cz>
2022-03-08 14:40:53 +0100

    Split error pages to no token and invalid token

--- src/main/java/com/buttercms/springstarterbuttercms/controller/ExceptionHandlingController.java
+++ src/main/java/com/buttercms/springstarterbuttercms/controller/ExceptionHandlingController.java
@@ -1,5 +1,6 @@
 package com.buttercms.springstarterbuttercms.controller;
 
+import com.buttercms.IButterCMSClient;
 import com.buttercms.exception.ButterCMSResponseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,9 +12,17 @@ public class ExceptionHandlingController {
 
     Logger logger = LoggerFactory.getLogger(ExceptionHandlingController.class);
 
+    private final IButterCMSClient butterCMSClient;
+
+    public ExceptionHandlingController(IButterCMSClient butterCMSClient) {
+        this.butterCMSClient = butterCMSClient;
+    }
+
     @ExceptionHandler(ButterCMSResponseException.class)
     public String invalidToken() {
-        logger.error("Your Butter token is set to an invalid value. Please verify your token is correct.");
-        return "404";
+        String token = butterCMSClient.getAuthToken();
+        // TODO - ask whether the same error message should be logged for both invalid and missing token -> it looks like it based on the react starter project
+        logger.error("Your Butter token might be set to an invalid value. Please verify your token is correct.");
+        return token == null ? "404" : "invalid-token";
     }
 }
